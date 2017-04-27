0a904ba9b70ffc3169a79886a2da8517b08f4fe8
nhanle2805 <nhanle285@gmail.com>
2017-04-27 20:38:02 +0700

    lan 1

---
+++ bai4.sh
@@ -0,0 +1,34 @@
+echo -e "menu \n"
+echo -e "1.cafe den"
+echo -e "2.cafe da"
+echo -e "2.cafe sua"
+echo -e "2.nuoc cam"
+echo " ban chon"
+read chon 
+if [$chon -eq 1] 
+	then
+
+	echo "cafe den";
+else
+
+
+if [$chon -eq 2] 
+	then  
+	echo "cafe d";
+else
+
+
+if [$chon -eq 3] 
+	then 
+ 	echo "cafe sua";
+else
+
+
+if [$chon -eq 4] 
+	then   
+	echo "nuoc cam";
+fi
+fi
+fi
+fi
+
